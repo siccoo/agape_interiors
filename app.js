c230bf0f8a9084748597c890a4cbdceca1d15a8b
siccoo <obichilaka@gmail.com>
2019-10-18 09:33:08 +0100

    Declaration of variables in app.js, cart in an array, getting products with class methods

---
+++ app.js
@@ -0,0 +1,22 @@
+// DECLARATION OF VARIABLES
+
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartContent = document.querySelector('.cart-content');
+const cartItem = document.querySelector('.cart-item');
+const cartTotal = document.querySelector('.cart-total');
+const productsDOM = document.querySelector('.products-center');
+
+// CART
+let cart = [];
+
+// GETTING THE PRODUCTS
+class Products {
+
+}
+
+// DISPLAYING(UI) OF THE PRODUCTS
+
