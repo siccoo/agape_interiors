4d1277291bfcf3b614286a10bcdd48d53108f469
siccoo <obichilaka@gmail.com>
2019-10-19 14:05:07 +0100

    Create an element to the addCartItem method func

--- app.js
+++ app.js
@@ -86,7 +86,8 @@ class UI {
                 // SET CART VALUES
                 this.setCartValues(cart);
 
-                // ADD / DISPLAY CART ITEM
+                // ADD CART ITEM TO THE DOM / DISPLAY CART ITEM
+                this.addCartItem(cartItem);
                 // SHOW THE CART
             });
         });
@@ -104,6 +105,24 @@ class UI {
         cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
         cartItems.innerText = itemsTotal
         console.log(cartTotal, cartItems);
+    }
+
+    addCartItem(item) {
+        const div = document.createElement("div");
+        div.classList.add("cart-item");
+        div.innerHTML = `
+            <img src=${item.image} alt="product">
+            <div>
+                <h4>${item.title}</h4>
+                <h5>#${item.price}</h5>
+                <span class="remove-item" data-id=${item.id}>remove</span>
+            </div>
+            <div>
+                <i class="fas fa-chevron-up"  data-id=${item.id}></i>
+                <p class="item-amount">${item.amount}</p>
+                <i class="fas fa-chevron-down"  data-id=${item.id}></i>
+            </div>
+        `;
     }
 }
 
